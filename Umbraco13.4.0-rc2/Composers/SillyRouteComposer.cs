feb7bc1da9154925b1649c01cdd03492184fc393
Kaspar Boel Kjeldsen <kaspar.kjeldsen@knowit.dk>
2024-06-19 14:06:18 +0200

    WIP

---
+++ Umbraco13.4.0-rc2/Composers/SillyRouteComposer.cs
@@ -0,0 +1,26 @@
+﻿using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Web.Common.ApplicationBuilder;
+using Umbraco13._4._0_rc2.Controllers;
+
+namespace Umbraco13._4._0_rc2.Composers
+{
+    public class SillyRouteComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder.Services.Configure<UmbracoPipelineOptions>(options =>
+            {
+                options.AddFilter(new UmbracoPipelineFilter(nameof(CustomVirtualPageController))
+                {
+                    Endpoints = app => app.UseEndpoints(e =>
+                    {
+                        // danish
+                        e.MapControllerRoute("CustomVirtualPageController", "/silly", new { Controller = "CustomVirtualPage", Action = "Index" });
+
+                    })
+                });
+            });
+
+        }
+    }
+}
---
+++ Umbraco13.4.0-rc2/Controllers/CustomVirtualPageController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace Umbraco13._4._0_rc2.Controllers
+{
+    public class CustomVirtualPageController : UmbracoPageController, IVirtualPageController
+    {
+        private readonly IUmbracoContextAccessor _umbracoContextAccessor;
+        public CustomVirtualPageController(ILogger<UmbracoPageController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) : base(logger, compositeViewEngine)
+        {
+            _umbracoContextAccessor = umbracoContextAccessor;
+        }
+
+        public IPublishedContent? FindContent(ActionExecutingContext actionExecutingContext)
+        {
+            _umbracoContextAccessor.TryGetUmbracoContext(out var umbracoContext);
+
+            return umbracoContext.Content.GetAtRoot().FirstOrDefault();
+        }
+
+        public IActionResult Index()
+        {
+            return View("VirtualPage", CurrentPage);
+        }
+    }
+}
